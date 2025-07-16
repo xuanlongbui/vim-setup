3f567d03e23fe13b63774e0ec6942cadd222d941
xuanlongbui <154042503+xuanlongbui@users.noreply.github.com>
2025-07-16 11:50:51 +0700

    Create vim_install.sh

---
+++ vim_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt install vim
+sudo apt install clangd
+sudo apt install nodejs npm
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sudo npm i -g yarn
+cd ~/.vim/plugged/coc.nvim
+yarn install
+
+cp ./.vimrc ~/.vimrc
